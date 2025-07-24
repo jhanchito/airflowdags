9f8b40ad334d039a10f824f7d592d27d649b6f35
Jhampier <jhampiertapia@gmail.com>
2025-07-24 18:07:19 +0000

    PRueba2

--- dags/airflow_monitoring.py
+++ dags/airflow_monitoring.py
@@ -1,22 +1,24 @@
-"""A liveness prober dag for monitoring composer.googleapis.com/environment/healthy."""
-import airflow
-from airflow import DAG
-from datetime import timedelta
+from airflow.models.dag import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
 
-default_args = {
-    'start_date': airflow.utils.dates.days_ago(0),
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5)
-}
+# Define a simple Python function that our task will execute
+def my_simple_python_function():
+    print("Hello from a Python function in Airflow!")
+    # You could add any Python logic here, e.g.,
+    # import pandas as pd
+    # df = pd.DataFrame({'col1': [1, 2], 'col2': [3, 4]})
+    # print(df)
 
-dag = DAG(
-    'airflow_monitoring',
-    default_args=default_args,
-    description='liveness monitoring dag',
-    schedule_interval='0 0 * * 0',
-    max_active_runs=2,
+with DAG(
+    dag_id='simple_python_dag',
+    start_date=datetime(2023, 1, 1),
+    schedule_interval=None,  # This DAG will only run manually
     catchup=False,
-    dagrun_timeout=timedelta(minutes=10),
-)
-
-# priority_weight has type int in Airflow DB, uses the maximum
+    tags=['example', 'python'],
+) as dag:
+    # Task 1: A simple PythonOperator that calls our function
+    run_python_task = PythonOperator(
+        task_id='execute_my_function',
+        python_callable=my_simple_python_function,
+    )
