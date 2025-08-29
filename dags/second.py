be3ee4742f852e10985f05d78103bdd2c530b178
Jhampier <jhampiertapia@gmail.com>
2025-08-29 18:00:17 +0000

    updated3

--- dags/second.py
+++ dags/second.py
@@ -5,7 +5,7 @@ from airflow.providers.standard.operators.empty import EmptyOperator
 
 my_dag = DAG(
      dag_id="my_dag_name",
-     start_date=datetime.datetime(2021, 1, 1),
+     start_date=datetime(2021, 1, 1),
      schedule="@daily",
  )
 EmptyOperator(task_id="task", dag=my_dag)
