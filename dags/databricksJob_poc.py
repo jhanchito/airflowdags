f206ab384baf5f7b91fa7291c350f58b08f1e486
Jhampier <jhampiertapia@gmail.com>
2025-10-10 01:30:52 +0000

    databricksPoC II

--- dags/databricksJob_poc.py
+++ dags/databricksJob_poc.py
@@ -1,11 +1,11 @@
 from airflow import DAG
 from airflow.providers.databricks.operators.databricks import DatabricksRunNowOperator
-from airflow.utils.dates import days_ago
+from datetime import datetime
 default_args = {
   'owner': 'airflow'
 }
 with DAG('databricksJob_poc',
-  start_date = days_ago(2),
+  start_date = datetime(2021, 1, 1),
   schedule_interval = None,
   default_args = default_args
   ) as dag:
