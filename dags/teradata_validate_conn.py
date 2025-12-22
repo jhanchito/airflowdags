39f30e450e447b78966eaf1342b8148dfb468149
Jhampier <jhampiertapia@gmail.com>
2025-12-22 13:53:43 -0500

    teradata Validate Con

---
+++ dags/teradata_validate_conn.py
@@ -0,0 +1,32 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.providers.teradata.hooks.teradata import TeradataHook
+from datetime import datetime
+
+def test_teradata_connection():
+    # Usamos el ID que ya tienes creado: 'teradata'
+    hook = TeradataHook(teradata_conn_id='teradata')
+    try:
+        # Intentamos obtener la conexión y ejecutar una consulta simple
+        conn = hook.get_conn()
+        print("¡Conexión exitosa al motor Teradata!")
+        
+        # Opcional: Validar versión o una tabla dual
+        df = hook.get_pandas_df(sql="SELECT DATABASE;")
+        print(f"Base de datos actual: {df.iloc[0,0]}")
+        
+    except Exception as e:
+        print(f"Error detectado durante la validación: {e}")
+        raise e
+
+with DAG(
+    dag_id='z_test_teradata_connection',
+    start_date=datetime(2023, 1, 1),
+    schedule_interval=None,
+    catchup=False
+) as dag:
+
+    test_task = PythonOperator(
+        task_id='validate_conn',
+        python_callable=test_teradata_connection
+    )
