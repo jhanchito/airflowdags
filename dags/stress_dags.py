8f9e112cca6d484a8c4439535843d21bfaa90ace
Jhampier <jhampiertapia@gmail.com>
2025-12-29 12:15:05 -0500

    stress proveII

--- dags/stress_dags.py
+++ dags/stress_dags.py
@@ -2,7 +2,7 @@ from airflow import DAG
 from airflow.providers.teradata.operators.teradata import TeradataOperator
 from datetime import datetime
 
-with DAG('stress_test_teradata_load', start_date=datetime(2023,1,1), schedule_interval='@once') as dag:
+with DAG('stress_test_teradata_load', start_date=datetime(2023,1,1), schedule='@once') as dag:
     for i in range(50):  # Genera 50 tareas paralelas
         TeradataOperator(
             task_id=f'query_teradata_{i}',
